eb79cabe338974c075d49f8547e32026c1b20c27
Chitresh Gurjar <chitresh.gurjar8779@gmail.com>
2025-11-22 12:09:43 +0530

    removing secret keys

--- devmate_backend/settings.py
+++ devmate_backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pgcpf16^qb+ziquu*#35u2ks46%c3bzw*!bp1*(1t5&s$!c2j0'
+SECRET_KEY = os.getenv("django_secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
